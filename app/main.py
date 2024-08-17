d7f14fe02e4e5c96a293156fdc2e7e2104eb0dc5
Diego Guedes <diegogdes@gmail.com>
2024-08-17 16:34:30 -0300

    Inserido informações no FastAPI

--- app/main.py
+++ app/main.py
@@ -5,7 +5,11 @@ from app.database import engine, Base
 # Inicializa o banco de dados
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Sistema de Recomendação de Filmes",
+    description="Uma API para gerenciar e recomendar filmes para um serviço de streaming.",
+    version="1.0.0",
+)
 
 # Inclui os routers
 app.include_router(filmes.router)
